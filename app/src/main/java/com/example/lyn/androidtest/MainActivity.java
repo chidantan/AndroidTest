88141045a21c606c35eda0067b6c45023a920b90
liu.yn <liuyuning@joyrry.com>
2018-06-05 16:54:13 +0800

    add

--- app/src/main/java/com/example/lyn/androidtest/MainActivity.java
+++ app/src/main/java/com/example/lyn/androidtest/MainActivity.java
@@ -1,10 +1,27 @@
 package com.example.lyn.androidtest;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageButton;
+import android.widget.Scroller;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
